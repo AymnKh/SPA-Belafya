839bea1803deb27a2ebc74794abdf1fc480a2838
AymanMoka <aymanmoka94@icloud.com>
2023-03-26 16:16:25 +0200

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import {SideComponent} from "./components/side/side.component";
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import {SideComponent} from "./components/side/side.component";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    SideComponent
+    NavbarComponent
+
   ],
   providers: [],
   bootstrap: [AppComponent]
